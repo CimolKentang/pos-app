08b76ce7cadcb5f21bebe59cd48b4f2480700c80
Cimol <cimolkentang245@gmail.com>
2025-06-22 11:25:01 +0700

    fix login screen

--- Views/Pages/Auth/LoginView.xaml.cs
+++ Views/Pages/Auth/LoginView.xaml.cs
@@ -1,3 +1,5 @@
+using Android.Content;
+using Android.Views.InputMethods;
 using inovasyposmobile.ViewModels.Auth;
 
 namespace inovasyposmobile.Views.Pages.Auth;
@@ -8,5 +10,27 @@ public partial class LoginView : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = loginViewModel;
+	}
+
+	private void UsernameCompleted(object sender, EventArgs e)
+	{
+		PasswordEntry.Focus();
+	}
+
+	private void UnfocusAllEntry(object sender, EventArgs e)
+	{
+		var context = Platform.CurrentActivity;
+        var inputMethodManager = context?.GetSystemService(Context.InputMethodService) as InputMethodManager;
+
+        if (inputMethodManager != null)
+        {
+            var activity = Platform.CurrentActivity;
+            var token = activity?.CurrentFocus?.WindowToken;
+            inputMethodManager.HideSoftInputFromWindow(token, HideSoftInputFlags.None);
+            activity?.Window?.DecorView.ClearFocus();
+        }
+		
+		// UsernameEntry.Unfocus();
+		// PasswordEntry.Unfocus();
 	}
 }
